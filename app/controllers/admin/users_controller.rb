60daa53a93ada0c3f2e8bdabd568332670a1120d
Stella Rouzi <differentreality@gmail.com>
2014-06-19 18:23:32 +0300

    fix users show page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,10 +5,24 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+
+    # Variable @show_attributes holds the attributes that are visible for the 'show' action
+    # If you want to change the attributes that are shown in the 'show' action of users
+    # add/remove the attributes in the following string array
+    @show_attributes = %w[name email affiliation biography registered attended created_at updated_at
+    sign_in_count current_sign_in_at last_sign_in_at current_sign_in_ip last_sign_in_ip]
+  end
+
   def update
     user = User.find(params[:id])
     user.update_attributes!(params[:user])
     redirect_to admin_users_path, :notice => "Updated #{user.email}"
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def delete
