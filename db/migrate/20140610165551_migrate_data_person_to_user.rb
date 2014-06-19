1f12f8cfe1a279a01e028a1ff73ea1e5edd9cb28
Stella Rouzi <differentreality@gmail.com>
2014-06-19 15:46:39 +0300

    fix migration, user name cannot be empty

--- db/migrate/20140610165551_migrate_data_person_to_user.rb
+++ db/migrate/20140610165551_migrate_data_person_to_user.rb
@@ -2,7 +2,11 @@ class MigrateDataPersonToUser < ActiveRecord::Migration
   def change
     Person.all.each do |p|
       user = User.find(p.user_id)
-      user.name = p.public_name
+      if p.public_name.empty?
+        user.name = p.email
+      else
+        user.name = p.public_name
+      end
       user.biography = p.biography
       user.save!
     end
