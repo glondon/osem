854a435244630009353b2788a10b292ca28f5cb2
Stella Rouzi <differentreality@gmail.com>
2015-03-02 11:35:19 +0200

    add require_registration in events#index events#show and change js for switch checkbox so that it works for any model and any attribute

--- app/assets/javascripts/osem-switch.js
+++ app/assets/javascripts/osem-switch.js
@@ -3,12 +3,16 @@ $(function () {
     $("[class='switch-checkbox']").bootstrapSwitch();
 
     $('input[class="switch-checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
-      url = "/admin/conference/" + this.name + "/events/" + this.value
+
+      //var conference = $(this).attr('conference');
+      var model = $(this).attr('model');
+      var attribute = $(this).attr('attribute');
+
+      url = '/admin/conference/' + this.name + '/' + model + '/' + this.value + '?event[' + attribute + ']=' + state
 
       $.ajax({
         url: url,
         type: 'PATCH',
-        data: { event: { is_highlight: state } },
         dataType: 'script'
       });
     });
