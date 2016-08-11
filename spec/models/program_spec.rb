f3826eafb8d38d05a9c4ca49ce8a0bbb396f634a
differentreality <differentreality@gmail.com>
2016-08-11 16:34:58 +0300

    voting period is open when voting dates are not set

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -90,7 +90,7 @@ describe Program do
 
   describe 'voting_period?' do
     it 'retuns false when voting dates are not set' do
-      expect(program.voting_period?).to eq false
+      expect(program.voting_period?).to eq true
     end
 
     shared_examples 'voting period' do |voting_start_date, voting_end_date, returns|
